3596a2faf650cc63b202ac4ed74fdaa34f137c76
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-05-09 16:06:01 +0100

    removed debug from example provider as well and update docblocks

--- src/Providers/Marketgoo/Data/Configuration.php
+++ src/Providers/Marketgoo/Data/Configuration.php
@@ -10,7 +10,6 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
 /**
  * @property-read string $api_url URL of the marketgoo API
  * @property-read string $api_key marketgoo API KEY for provision API
- * @property-read bool|null $debug Whether or not to log all HTTP requests and responses
  */
 class Configuration extends DataSet
 {
